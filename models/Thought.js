30512cc7fc3b6f2c9732f8eb9370b8a6cc9c6feb
Abed Hasan <abedhasan79@gmail.com>
2022-11-07 17:20:23 -0500

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const {Schema, model} = require('mongoose');
+const Reaction = require('./Reaction.js');
+
+//Schema for what makes up a thought
+const thoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            validate:{
+                minlength: 1,
+                maxlength: 280,
+            },
+        },
+
+        createdAt:{
+            type: Date,
+            default: Date.now,
+        },
+
+        username:{
+            type: String,
+            required: true,
+        },
+
+        reactions:[Reaction],
+    },
+
+    {
+        toJSON:{
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+//Create a virtual property 'reactionCount' that gets a thoughts total number of reaction
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+// initialize thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
