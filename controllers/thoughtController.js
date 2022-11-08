0d60e7fdecab71ac0b5fb6688bae67b8b29d9468
Abed Hasan <abedhasan79@gmail.com>
2022-11-07 22:24:16 -0500

    implimented getSingleThought, updateThought and deleteThought in thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,6 +1,7 @@
 const { User, Thought, Reaction } = require('../models');
 
 module.exports = {
+    //get all thoughts
     getThoughts(req, res) {
         Thought.find({})
             .select('-__v')
@@ -8,6 +9,7 @@ module.exports = {
             .catch((err) => res.json(err));
     },
 
+    //create a thought
     createThought(req, res) {
         Thought.create(req.body)
             .then((thoughts) => {
@@ -20,4 +22,36 @@ module.exports = {
             .then((thoughts) => !thoughts ? res.status(404).json({ message: 'thoughts created but no user with that id' }) : res.json(thoughts))
             .catch((err) => res.json(err));
     },
+
+    //get a thought bu its id
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thoughts) => !thoughts ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //update a thought by it's id
+    updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { thoughtText: req.body.thoughtText }, { new: true })
+            .select('-__v')
+            .then((thoughts) => !thoughts ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //delete a thought by it's id
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thoughts) => !thoughts ? res.status(404).json({ message: 'No thought with that ID' })
+                : User.findOneAndUpdate(
+                    { thoughts: req.params.thoughtId },
+                    { $pull: { thoughts: req.params.thoughtId } },
+                    { new: true },
+                )
+            )
+            .then((user) => !user ? res.status(404).json({ message: 'No user with this id' }) : res.json({ message: 'thougt deleted' }))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //create a reaction
 };
--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-const {getThoughts, createThought} = require('../../controllers/thoughtController');
+const {getThoughts, createThought, getSingleThought, updateThought, deleteThought} = require('../../controllers/thoughtController');
 
 router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 
 
